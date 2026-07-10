cf8e8e27b214ecae1d68ab383921cab93d0a2eb3
siby667 <siby@trybaskt.com>
2026-07-09 22:43:15 -0400

    added opensearch version

--- infrastructure_terraform/environments/dev/providers.tf
+++ infrastructure_terraform/environments/dev/providers.tf
@@ -10,8 +10,9 @@ data "aws_caller_identity" "current" {}
 data "aws_partition" "current" {}
 
 provider "opensearch" {
-  url               = "https://${var.opensearch_endpoint_override != "" ? var.opensearch_endpoint_override : module.opensearch_domain.endpoint}"
-  aws_region        = var.aws_region
-  sign_aws_requests = true
-  healthcheck       = false
+  url                = "https://${var.opensearch_endpoint_override != "" ? var.opensearch_endpoint_override : module.opensearch_domain.endpoint}"
+  aws_region         = var.aws_region
+  sign_aws_requests  = true
+  healthcheck        = false
+  opensearch_version = "3.5"
 }
