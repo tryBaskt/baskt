ae9800c674979a15771b1eebf01e27785d6d1623
siby667 <siby@trybaskt.com>
2026-07-10 11:53:48 -0400

    market control lambda will check market hours after worker deployed
    market control lambda will check market hours after worker deployed

--- infrastructure_terraform/dev/modules/trade_worker/lambda_event_source_trade_execution_queue.tf
+++ infrastructure_terraform/dev/modules/trade_worker/lambda_event_source_trade_execution_queue.tf
@@ -4,4 +4,8 @@ resource "aws_lambda_event_source_mapping" "trade_execution_queue" {
   batch_size              = 10
   enabled                 = false
   function_response_types = ["ReportBatchItemFailures"]
+
+  lifecycle {
+    ignore_changes = [enabled]
+  }
 }
